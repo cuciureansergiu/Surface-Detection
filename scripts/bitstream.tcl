e76db588a06ec1bb50057d14a5c41ac99560ff70
Sergiu <cuciurean.sergiu@yahoo.com>
2019-06-15 01:31:05 +0300

    Added device tree include file Removed DMA' scatter gather

--- scripts/bitstream.tcl
+++ scripts/bitstream.tcl
@@ -1,5 +1,10 @@
 #!/usr/bin/tclsh
+# Set the reference directory for source file relative paths (by default the value is script directory path)
+set origin_dir ".."
+# Set the directory path for the original project from where this script was exported
+set bin_dir "[file normalize "$origin_dir/bin"]"
+reset_run synth_1
 launch_runs impl_1 -to_step write_bitstream -jobs 6 
 wait_on_run impl_1
 open_run impl_1
-write_bitstream System.bit
+write_bitstream $bin_dir/System.bit
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 export CROSS_COMPILE=arm-linux-gnueabihf-
 export ARCH=arm
 source /tools/Xilinx/Vivado/2018.3/settings64.sh
